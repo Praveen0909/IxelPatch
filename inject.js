b6dacce114279925e5193d5e21845c55f57b364b
shekjbs <shekjbs@gmail.com>
2017-07-09 11:54:46 +0530

    Updated Changes

--- inject.js
+++ inject.js
@@ -1,5 +1,5 @@
 var s=document.createElement("script");
-var a="var a=document.getElementsByTagName('body')[0];  a.addEventListener('paste',function(e){   var b=e.clipboardData.getData('text/plain');         codeEditor.insert(b);                              }); a.addEventListener('click',function(){     var d=document.getElementById('rdialog');    if(d!=null){ console.log(d);        d.removeAttribute('id');         }});";
+var a="var a=document.getElementsByTagName('body')[0];  a.addEventListener('paste',function(e){   var b=e.clipboardData.getData('text/plain');         codeEditor.insert(b);                              }); a.addEventListener('click',function(){     var d=document.getElementById('rdialog');    if(d!=null){        d.removeAttribute('id');         }});";
 
 s.appendChild(document.createTextNode(a));
 s.onload = function() {
